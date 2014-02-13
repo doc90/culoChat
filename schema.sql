4fdbf808c21a37826c296735fe4d833fa480b2b2
doc90 <filippo.giomi@gmail.com>
2014-02-13 13:56:50 +0100

    aggiornata creazione schema

--- schema.sql
+++ schema.sql
@@ -1,7 +1,30 @@
-CREATE DATABASE `chat` /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE DATABASE  IF NOT EXISTS `chat` ;
+USE `chat`;
 
-DROP TABLE IF EXISTS `chat`.`messages`;
-CREATE TABLE  `chat`.`messages` (
+CREATE TABLE `group_permission` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `idgroup` int(11) DEFAULT NULL,
+  `idpermission` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+LOCK TABLES `group_permission` WRITE;
+INSERT INTO `group_permission` VALUES (1,3,1),(2,3,2);
+UNLOCK TABLES;
+
+
+CREATE TABLE `groups` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+LOCK TABLES `groups` WRITE;
+INSERT INTO `groups` VALUES (1,'User'),(2,'Mod'),(3,'Admin');
+UNLOCK TABLES;
+
+
+CREATE TABLE `messages` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `userid` int(10) unsigned NOT NULL,
   `timestamp` datetime NOT NULL,
@@ -9,8 +32,28 @@ CREATE TABLE  `chat`.`messages` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=89 DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `chat`.`users`;
-CREATE TABLE  `chat`.`users` (
+
+
+CREATE TABLE `permissions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `permission` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+LOCK TABLES `permissions` WRITE;
+INSERT INTO `permissions` VALUES (1,'edit'),(2,'delete');
+UNLOCK TABLES;
+
+
+CREATE TABLE `user_groups` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `iduser` int(11) DEFAULT NULL,
+  `idgroup` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `users` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `nick` varchar(45) NOT NULL,
   `pass` varchar(100) NOT NULL,
